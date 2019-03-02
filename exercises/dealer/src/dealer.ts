7bb73eb3a23b0659a318fca3ffee44722320a022
JuliaNeumann <jn.julianeumann@gmail.com>
2019-03-02 20:12:47 +0100

    challenge 3: dealer

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,40 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs, Diamonds, Hearts, Spades
+}
 
-// }
+export enum CardNumber {
+  Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+}
+
+export type Card = [Suit, CardNumber];
+
+export class Dealer {
+  deck: Card[] = [];
+
+  constructor() {
+    for (let i = 0; i < Object.keys(Suit).length / 2; i++) {
+        for (let j = 0; j < Object.keys(CardNumber).length / 2; j++) {
+          this.deck.push([i, j]);
+        }
+    }
+    shuffleArray(this.deck);
+  }
+
+  dealHand(num: number): Card[] {
+    if (num > this.getLength() || num < 0) {
+      throw new Error();
+    }
+    return this.deck.splice(0, num);
+  }
+
+  getLength(): number {
+    return this.deck.length;
+  }
+
+  readCard(card: Card): string {
+    return `${CardNumber[card[1]]} of ${Suit[card[0]]}`;
+  }
+}
