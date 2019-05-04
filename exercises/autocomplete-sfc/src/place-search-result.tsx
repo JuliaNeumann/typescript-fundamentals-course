ebd71e7c07292eeac327d6dcc67e0d1f2a6628b2
JuliaNeumann <jn.julianeumann@gmail.com>
2019-05-04 11:23:35 +0200

    autocomplete sfc - fix formatting

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -3,30 +3,18 @@ import { PlaceDetails } from './utils/places';
 
 export const PlaceSearchResult: React.SFC<PlaceDetails> = (props) => {
   return (
-      <li
-          className="search-result"
-      >
-          <img
-              className="icon"
-              src={props.icon}
-          />
-          <h3>
-
-              {props.name}
-
-          </h3>
+      <li className="search-result">
+          <img className="icon"
+               src={props.icon} />
+          <h3>{props.name}</h3>
           <p>
-              <a
-                  href={props.url}
-                  target="_blank"
-              >
+              <a href={props.url}
+                 target="_blank">
                   {props.vicinity}
               </a>
               -
-              <a
-                  href={props.website}
-                  target="_blank"
-              >
+              <a href={props.website}
+                 target="_blank">
                   {props.website}
               </a>
           </p>
