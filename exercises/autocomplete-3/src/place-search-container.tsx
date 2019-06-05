1374fa505793b57aa12c39f865fc0c38d3c5236d
JuliaNeumann <jn.julianeumann@gmail.com>
2019-06-05 20:56:21 +0200

    autocomplete 3 - unfinished

--- exercises/autocomplete-3/src/place-search-container.tsx
+++ exercises/autocomplete-3/src/place-search-container.tsx
@@ -4,12 +4,17 @@ import { shortUrl } from './utils/string';
 import { PlaceDetails } from './utils/places';
 import { PlaceSearchResult } from './place-search-result';
 import { PlaceSearchResultList } from './place-search-result-list';
+import {IPlaceSearchResultList} from "../../autocomplete-2/src/place-search-result-list";
 
 
-export class PlaceSearchContainer extends React.Component<{}, {}> {
+export class PlaceSearchContainer extends React.Component<{}, IPlaceSearchResultList> {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+        results: [],
+        term: '',
+        inProgress: false
+    };
     // Event handler for changes to search term
     this.beginSearch = this.beginSearch.bind(this);
   }
@@ -21,7 +26,10 @@ export class PlaceSearchContainer extends React.Component<{}, {}> {
    * @memberof PlaceSearch
    * @return {undefined}
    */
-  beginSearch(term: string) {
+  async beginSearch(term: string) {
+    this.setState({inProgress: true});
+    let results = await autocomplete(term);
+    this.setState({ results, inProgress: false });
     // Initiate a search using the ./autocomplete.ts module
     // When the promise it returns resolves, update your state accordingly
   }
@@ -37,8 +45,10 @@ export class PlaceSearchContainer extends React.Component<{}, {}> {
    */
   render() {
     return (
-      <p>Replace this with a PlaceSearchResultList</p>
-      // <PlaceSearchResultList />
+        <PlaceSearchResultList onInput={this.beginSearch}
+                               inProgress={this.state.inProgress}
+                               results={this.state.results}
+                               term={this.state.term} />
     );
   }
 }
--- exercises/autocomplete-3/src/utils/places.ts
+++ exercises/autocomplete-3/src/utils/places.ts
@@ -1,16 +1,26 @@
+var autocompleteResult = require('./autocomplete.json');
+var detailsResult = require('./details.json');
+
 export interface PlaceSummary {
-  description: string;
-  id: string;
-  place_id: string;
+    description: string;
+    id: string;
+    place_id: string;
 }
 
 export interface PlaceDetails {
-  rating: number;
-  icon: string;
-  name: string;
-  url: string;
-  vicinity: string;
-  website?: string;
+    id: string;
+    rating: number;
+    icon: string;
+    name: string;
+    url: string;
+    vicinity: string;
+    website?: string;
+}
+
+function delay(t:number, v?: any) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, v), t)
+    });
 }
 
 /**
@@ -21,23 +31,23 @@ export interface PlaceDetails {
  * @returns {Promise} promise that resolves to search results
  */
 export function fetchPlaceSummaries(input: string): Promise<PlaceSummary[]> {
-  return fetch(
-    `http://localhost:3000/maps/api/place/autocomplete/json?types=establishment&input=${input}`
-  )
-    .then(response => response.json())
-    .then(jsonData => {
-      return jsonData.predictions as PlaceSummary[];
-    });
+    return fetch(
+        `http://localhost:3000/maps/api/place/autocomplete/json?types=establishment&input=${input}`
+    )
+    //.then(response => response.json())
+        .then(jsonData => {
+            return delay(1000).then(() => input ? autocompleteResult.predictions as PlaceSummary[] : []);
+        });
 }
 
 export function fetchPlaceDetails(placeids: string[]): Promise<PlaceDetails[]> {
-  return Promise.all(
-    placeids.map(placeid => {
-      return fetch(
-        `http://localhost:3000/maps/api/place/details/json?placeid=${placeid}`
-      )
-        .then(response => response.json())
-        .then(jsonData => jsonData.result as PlaceDetails);
-    })
-  );
+    return Promise.all(
+        placeids.map(placeid => {
+            return fetch(
+                `http://localhost:3000/maps/api/place/details/json?placeid=${placeid}`
+            )
+            //.then(response => response.json())
+                .then(jsonData => detailsResult.result as PlaceDetails);
+        })
+    );
 }
